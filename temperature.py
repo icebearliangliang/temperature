fe7ca51c8b143ad8b42109b72b091fb003f2c244
icebear <icebearliangliang@163.com>
2019-04-12 15:22:59 +0800

    add new file

---
+++ temperature.py
@@ -0,0 +1,5 @@
+temperature = input('please input outside temperature: ')
+temperature = float(temperature)
+#fahrenheit 华氏温度
+fahrenheit = 32 + temperature * 1.8
+print('fahrenheit is: ', fahrenheit)
